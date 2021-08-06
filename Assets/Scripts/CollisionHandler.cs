b674af257802d2161b76dab6087aec3a4254e678
Atharva Akhare <atharva.akhare05@outlook.com>
2021-08-06 15:58:45 +0530

    no message

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-            
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly object hit");
+                break;
+            case "Finish":
+                Debug.Log("Level finished!");        
+                break;            
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
